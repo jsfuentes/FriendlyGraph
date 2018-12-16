f8a25055828dcd4b9ae5e30b76d8ab65de5abeac
Jorge Fuentes <jsjfuentesj@gmail.com>
2018-12-15 19:49:22 -0800

    Add some fun new fields

--- visualizer/src/main/utils.py
+++ visualizer/src/main/utils.py
@@ -4,5 +4,10 @@ PATH = "../parser/result.json"
 def getFriendData():
     with open(PATH) as f:
         data = json.load(f)
+    result = []
+    for k, v in data.items():
+        v["msg_diff"] = v["number_received"]-v["number_sent"]
+        v["word_diff"] = v["words_received"]-v["words_sent"]
+        result.append(v)
     
-    return list(data.values())
+    return result
