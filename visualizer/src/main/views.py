3e6a3b791345af0cd560d6376ec906a5c7d566d2
Jorge Fuentes <jsjfuentesj@gmail.com>
2018-12-15 18:58:14 -0800

    Add base table

--- visualizer/src/main/views.py
+++ visualizer/src/main/views.py
@@ -2,7 +2,33 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 main = Blueprint('main', __name__)
 
+def getSampleData():
+    data = {
+        "Hansoo Kim": {
+          "number_sent": 1,
+          "dict_sent": {
+            "desk!!": 1,
+            "interested": 1,
+            "in": 1,
+            "I'm": 1,
+            "Hi,": 1,
+            "your": 1,
+            "buying": 1
+          },
+          "words_sent": 40,
+          "dict_received": {},
+          "number_received": 0,
+          "friend_coef": 41,
+          "words_received": 0
+        }
+    }
+    finalData = []
+    for key, value in data.items():
+        value["name"] = key 
+        finalData.append(value)
+    return finalData 
+    
 @main.route('/')
 def index():
-    return render_template("index.html")
-
+    friends = getSampleData()
+    return render_template("index.html", friends = friends)
