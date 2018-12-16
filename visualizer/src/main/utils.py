ec19810bbb45c9d671704a9b532e65898749efc8
Jorge Fuentes <jsjfuentesj@gmail.com>
2018-12-15 20:06:03 -0800

    add environment

--- visualizer/src/main/utils.py
+++ visualizer/src/main/utils.py
@@ -8,6 +8,18 @@ def getFriendData():
     for k, v in data.items():
         v["msg_diff"] = v["number_received"]-v["number_sent"]
         v["word_diff"] = v["words_received"]-v["words_sent"]
+        maxCount = 0
+        maxWord = None
+        for word, count in v["dict_sent"].items():
+            if count > maxCount:
+                maxCount = count
+                maxWord = word 
+        v["most_common_sent_word"] = maxWord
+        for word, count in v["dict_received"].items():
+            if count > maxCount:
+                maxCount = count
+                maxWord = word 
+        v["most_common_received_word"] = maxWord
         result.append(v)
     
     return result
